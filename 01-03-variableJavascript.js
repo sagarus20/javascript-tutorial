2db5392caf0bff29f18ead0e71ae3e080aaf7be2
sagarus20 <juanpablocardenasq@gmail.com>
2025-03-12 18:21:51 -0500

    comparacion entre variables

---
+++ 01-03-variableJavascript.js
@@ -0,0 +1,24 @@
+ let a 
+ let esPar=function(a){
+    if(a%2==0){
+       return true
+    }
+    else{
+       return false
+    }
+   }
+   
+   a=7
+   console.log(esPar(a))
+   const adulto=18
+   let esMayor=function(a){
+    if(a>adulto){
+       return true
+    }
+    else{
+       return false
+    }
+   }
+   
+   a=21
+   console.log(esMayor(a))
---
+++ 02-01-tiposdedato.js
@@ -0,0 +1,15 @@
+var nombre ='Juan'
+var apellido='perez'
+
+var nombreCompleto = nombre +' '+ apellido
+console.log(nombreCompleto)
+var nombreCompleto2='Carlos' +' '+ 'lara'
+console.log(nombreCompleto2)
+
+var x =nombre +2 +19
+console.log(x)
+
+x =nombre +(2 +19)
+console.log(x)
+x = 2 +19 +nombre
+console.log(x)
